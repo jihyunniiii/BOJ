a8586ae5080ce792550f44fd2330721f0ab6b57e
jihyunniiii <103172971+jihyunniiii@users.noreply.github.com>
2022-07-30 15:07:33 +0900

    Add files via upload

---
+++ 1010.py
@@ -0,0 +1,15 @@
+# 1010번 다리 놓기
+
+fact = [0] * 31
+fact[0] = 1
+
+for i in range(1, 31):
+  fact[i] = fact[i - 1] * i
+  
+T = int(input())
+
+lists = [input() for _ in range(T)]
+
+for i in lists:
+  N, M = map(int, i.split())
+  print(int(fact[M] / (fact[M - N] * fact[N])))
